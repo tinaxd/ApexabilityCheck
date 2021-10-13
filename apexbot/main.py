a9ba51e9c09a8e6d8762e9ca8573d1e64b008f75
tinaxd <31384595+tinaxd@users.noreply.github.com>
2021-10-14 01:36:32 +0900

    complete

--- apexbot/main.py
+++ apexbot/main.py
@@ -1,8 +1,10 @@
 import discord # type: ignore
 import os
 import sys
-from typing import cast, Optional
+from typing import cast, Optional, Tuple, Union
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 try:
     TOKEN = os.environ['DISCORD_TOKEN']
@@ -19,49 +21,77 @@ intents.presences = True
 client = discord.Client(intents=intents)
 
 def _find_channel(guild: discord.Guild, chan_name: str) -> Optional[discord.TextChannel]:
-    last_text_channel = None
     for chan in guild.channels:
         if not isinstance(chan, discord.TextChannel):
             continue
         if chan.name == chan_name:
             return chan
-        last_text_channel = chan
-    print(f'could not find #{chan_name} in {guild.name}')
-    return last_text_channel
+    logging.info(f'could not find #{chan_name} in {guild.name}')
+    return None
 
-async def _send_member_online_notification(member: discord.Member) -> None:
-    guild = member.guild
-    chan = _find_channel(guild, 'online-status')
-    if not chan:
-        return
-    content = f'{member.display_name} がオンラインになりました'
-    await chan.send(content=content)
+NOTIFYCHAN = 'apexability-check'
 
-async def _send_start_game_notification(member: discord.Member) -> None:
+async def _send_apex_notification(member: discord.Member, game: str, is_start: bool) -> None:
     guild = member.guild
-    playing = cast(discord.Game, member.activity).name
-    chan = _find_channel(guild, 'online-status')
+    chan = _find_channel(guild, NOTIFYCHAN)
     if chan:
-        content = f'{member.display_name} が {playing} を始めました'
+        if is_start:
+            tail = 'を始めました！'
+        else:
+            tail = 'をやめました！'
+        content = f'{member.display_name} が {game} {tail}'
         await chan.send(content=content)
+
+ActType = Union[discord.BaseActivity, discord.Spotify]
+APEXGAME = "Apex Legends"
+
+def apex_started(oldActs: Tuple[ActType], newActs: Tuple[ActType]) -> bool:
+    # APEX はプレイしていなかったことを確認
+    for act in oldActs:
+        if (isinstance(act, discord.Spotify)):
+            continue
+        if act.type == discord.ActivityType.playing and act.name == APEXGAME:
+            return False
+    
+    # 今 APEX をプレイしていることを確認
+    for act in newActs:
+        if (isinstance(act, discord.Spotify)):
+            continue
+        if act.type == discord.ActivityType.playing and act.name == APEXGAME:
+            return True
+    return False
+
+def apex_stopped(oldActs: Tuple[ActType], newActs: Tuple[ActType]) -> bool:
+    # APEX をプレイしていたことを確認
+    apexed = False
+    for act in oldActs:
+        if (isinstance(act, discord.Spotify)):
+            continue
+        if act.type == discord.ActivityType.playing and act.name == APEXGAME:
+            apexed = True
+            break
+    if not apexed:
+        return False
+    
+    # 今 APEX をプレイしていないことを確認
+    for act in newActs:
+        if (isinstance(act, discord.Spotify)):
+            continue
+        if act.type == discord.ActivityType.playing and act.name == APEXGAME:
+            return False
+    return True
 
 @client.event
 async def on_member_update(before: discord.Member, after: discord.Member) -> None:
-    print(f'member update ${str(before)} ${after}')
-    if isinstance(after.status, str):
+    logging.debug(f'member update ${str(before)} ${after}')
+    
+    started = apex_started(before.activities, after.activities)
+    stopped = apex_stopped(before.activities, after.activities)
+    if not started and not stopped:
+        logging.debug("not related to apex")
         return
-    if after.status == discord.Status.online \
-       and before.status != discord.Status.online:
-        #await _send_member_online_notification(after)
-        pass
-    elif after.activity.type == discord.ActivityType.playing:
-        after_game = after.activity.name
-        if before.activity.type == discord.ActivityType.playing:
-            before_game = before.activity.name
-            if after_game != before_game:
-                await _send_start_game_notification(after)
-        else:
-            await _send_start_game_notification(after)
+    
+    await _send_apex_notification(after, APEXGAME, started)
 
 
 client.run(TOKEN)
